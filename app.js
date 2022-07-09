af46e3129b8fb2dc38b7e5ff24a1364fdb4463bb
frass-stack <60819298+frass-stack@users.noreply.github.com>
2022-07-09 13:48:57 -0300

    Primeros bocetos de la app: titulo e import al main

---
+++ app.js
@@ -0,0 +1,15 @@
+require('colors');
+
+const { mostrarMenu } = require('./helpers/mensajes');
+
+
+
+const main = async () => {
+
+    console.log(`Hola mundo`);
+
+    mostrarMenu();
+
+}
+
+main();
---
+++ helpers/mensajes.js
@@ -0,0 +1,12 @@
+require('colors');
+
+const mostrarMenu = ()=>{
+    console.clear();
+    console.log('===========================');
+    console.log('   Seleccione una opcion   ');
+    console.log('===========================\n');
+}
+
+module.exports = {
+    mostrarMenu
+}
