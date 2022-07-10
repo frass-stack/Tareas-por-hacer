bc6c54ef9cb92cefad15d4bc93331b1cd468ac9c
frass-stack <60819298+frass-stack@users.noreply.github.com>
2022-07-09 22:24:52 -0300

    mostrarMenu y pausa, pasan a ser promise

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,44 +1,55 @@
 require('colors');
 
 const mostrarMenu = ()=>{
-    console.clear();
-    console.log('==========================='.green);
-    console.log('   Seleccione una opcion   '.green);
-    console.log('===========================\n'.green);
 
-    console.log(`${'1.'.green} Crear tarea`)
-    console.log(`${'2.'.green} Listar tareas`)
-    console.log(`${'3.'.green} Listar tareas completadas`)
-    console.log(`${'4.'.green} Listar tareas pendientes`)
-    console.log(`${'5.'.green} Completar tarea(s)`)
-    console.log(`${'6.'.green} Borrar tarea`)
-    console.log(`${'0.'.green} Salir \n`)
-
-    //Preparamos la interfaz que se le presentara al usuario.
-    const readLine = require('readline').createInterface({
-        //Pausamos para recibir la entrada del usuario.
-        input: process.stdin,
-        //Pausamos para emitir la respuesta al usuario.
-        output: process.stdout
+    return new Promise((resolve)=>{
+        console.clear();
+        console.log('==========================='.green);
+        console.log('   Seleccione una opcion   '.green);
+        console.log('===========================\n'.green);
+    
+        console.log(`${'1.'.green} Crear tarea`)
+        console.log(`${'2.'.green} Listar tareas`)
+        console.log(`${'3.'.green} Listar tareas completadas`)
+        console.log(`${'4.'.green} Listar tareas pendientes`)
+        console.log(`${'5.'.green} Completar tarea(s)`)
+        console.log(`${'6.'.green} Borrar tarea`)
+        console.log(`${'0.'.green} Salir \n`)
+    
+        //Preparamos la interfaz que se le presentara al usuario.
+        const readLine = require('readline').createInterface({
+            //Pausamos para recibir la entrada del usuario.
+            input: process.stdin,
+            //Pausamos para emitir la respuesta al usuario.
+            output: process.stdout
+        })
+    
+        readLine.question(`\nSelecciones una opcion: `, (opcion)=>{
+            readLine.close();
+            resolve(opcion);
+        })
     })
 
-    readLine.question(`\nSelecciones una opcion: `, (opcion)=>{
-        readLine.close();
-    })
 }
 
 const pausa = ()=> {
-    //Preparamos la interfaz que se le presentara al usuario.
-    const readLine = require('readline').createInterface({
-        //Pausamos para recibir la entrada del usuario.
-        input: process.stdin,
-        //Pausamos para emitir la respuesta al usuario.
-        output: process.stdout
-    })
 
-    readLine.question(`\nPresione ${'ENTER'.green}\n`, (opcion)=>{
-        readLine.close();
-    })
+    //Vamos a convertir nuestras funciones en promise, pero no usaremos de momento el reject.
+    return new Promise ( (resolve) => {
+        //Preparamos la interfaz que se le presentara al usuario.
+        const readLine = require('readline').createInterface({
+            //Pausamos para recibir la entrada del usuario.
+            input: process.stdin,
+            //Pausamos para emitir la respuesta al usuario.
+            output: process.stdout
+        })
+    
+        readLine.question(`\nPresione ${'ENTER'.green}\n`, (opcion)=>{
+            readLine.close();
+            resolve();
+        })
+    }) 
+
 }
 
 module.exports = {
