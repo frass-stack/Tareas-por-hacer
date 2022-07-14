79a8f8277f5a1b112009a1c7c1dd503522e637f3
frass-stack <60819298+frass-stack@users.noreply.github.com>
2022-07-14 00:02:55 -0300

    metodo leerInput, getter listadoArr, cambios menores

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 require('colors');
-const { inquirerMenu, inquirerPausa } = require('./helpers/inquirer');
+const { inquirerMenu, inquirerPausa, leerInput } = require('./helpers/inquirer');
 const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 
 const main = async () => {
 
     let opcion = '';
+    const tareas = new Tareas();
 
     do {
+        //Imprime el menu
         opcion = await inquirerMenu();
-        //console.log({ opcion })
 
+        switch (opcion) {
+            case '1':
+                const descripcion = await leerInput('Descripcion:');
+                tareas.crearTarea(descripcion);
+                break;
+            case '2':
+                console.log(tareas.listadoArr);
+                break;
+        }
+        
         console.log('\n')
         if(opcion !== '0') await inquirerPausa();
         
--- models/tareas.js
+++ models/tareas.js
@@ -1,13 +1,29 @@
-
+const Tarea = require('./tarea');
 
 
 class Tareas {
 
     _listado = {};
 
-    constructor(){
+    //getter
+    get listadoArr(){
+        const listado = [];
+        //Extras todas las llaves de la propiedad _listado, en un arreglo de Strings
+        Object.keys(this._listado).forEach((key)=>{
+            const tarea = this._listado[key];
+            //Añadimos las tareas que esten instanciadas al listado y lo retornamos.
+            listado.push(tarea);
+        })
+        return listado;
+    }
 
+    constructor(){
         this._listado = {};
+    }
+
+    crearTarea = (descripcion = '') => {
+        const tarea = new Tarea(descripcion);
+        this._listado[tarea.id] = tarea;
     }
 }
 
